180bade2de12f1d17a82b5bf55ed64c6ec0a0ff2
pocomane <none@mail.com>
2020-10-16 16:50:24 +0200

    Code init.

---
+++ hook/action/start.sh
@@ -0,0 +1,19 @@
+#/usr/bin/env bash
+
+EXE_NAME="WebMenu"
+EXE_PATH="../MiSTer_WebMenu"
+
+die(){
+  echo "ERROR $1"
+  exit 127
+}
+
+PID=$(ps ax -o pid,comm | grep "$EXE_NAME"| grep -oh '^[ \t]*[0-9]*' | grep -oh '[0-9]*')
+
+if [ "$PID" != "" ]; then
+  false ||die "there is an instance already running"
+fi
+( (nohup $EXE_PATH 8081 > "./$EXE_NAME.log" 2>&1) &) ||die "can not start the instance"
+sleep 1
+tail  "./$EXE_NAME.log"
+
